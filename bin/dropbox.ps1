ef6c9b8816efd2224e9b269f75880ab94a3fba73
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-05-15 22:19:35 +0200

    Push and pop location

--- bin/dropbox.ps1
+++ bin/dropbox.ps1
@@ -2,8 +2,12 @@ param (
     [Parameter(Mandatory=$false)][string]$dir = [IO.Path]::Combine($HOME, "Dropbox", "Statistics", "Arbetsformedlingen"),
     [Parameter(Mandatory=$true)][string]$command
 )
-$prev = $PWD
-$arbetsformedlingen = [IO.Path]::Combine($PSScriptRoot, ".." , "src", "Stacka.Arbetsformedlingen")
-Set-Location $arbetsformedlingen
-dotnet run --dir $dir --command $command
-Set-Location $prev
+Push-Location
+try {
+    $arbetsformedlingen = [IO.Path]::Combine($PSScriptRoot, ".." , "src", "Stacka.Arbetsformedlingen")
+    Set-Location $arbetsformedlingen
+    dotnet run --dir $dir --command $command 
+}
+finally {
+    Pop-Location
+}
