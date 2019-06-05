bb071bba45c0379c6682d1f2c4c8c8b0fc88591b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-06-05 20:00:11 +0200

    Thoughtworks radar

---
+++ src/Stacka.Thoughtworks/Library.fs
@@ -0,0 +1,12 @@
+﻿namespace Stacka.Thoughtworks
+open System
+open FSharp.Data
+
+module private Internal=
+  type T =HtmlProvider<"./A-Z_Technology_Radar_ThoughtWorks.html">
+
+
+module Say =
+//A-Z_Technology_Radar_ThoughtWorks.html
+    let hello name =
+        printfn "Hello %s" name
